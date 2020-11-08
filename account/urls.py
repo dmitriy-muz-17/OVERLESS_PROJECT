2b36fdef5c792ed74fd6564a6c5512c991f43af6
Dmitriy <muzichuckdima@gmail.com>
2020-11-08 13:51:11 +0200

    Project work perfect

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+ #path('login/', views.user_login, name='login'), моя реализация аунтификаци ( основной минус плохая оптимизация)
+ path('login/', auth_views.LoginView.as_view(), name='login'),
+ path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+ path('', views.dashboard, name='dashboard'),
+ path('password_change', auth_views.PasswordChangeView.as_view(), name='password_change'),
+ path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+ path('edit/', views.edit, name='edit'),
+ path('register/', views.register, name='register')
+ ]
