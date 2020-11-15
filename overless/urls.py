ce6de68d7d7cf3db7204e30a64d0a90ec0764d1c
Dmitriy <muzichuckdima@gmail.com>
2020-11-15 18:43:05 +0200

    Валидация полей формы Переопределение метода save() модельной формы Добавил веб страницу для добавления фотографии(поста) в базу данных

--- overless/urls.py
+++ overless/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ]
 
 if settings.DEBUG:
